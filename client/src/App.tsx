cfd44acec060b6c8daabcc411dd5251dc5d6d7c1
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2026-05-13 22:31:59 +0530

    Forget Password  Button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import Appraisal from './pages/Appraisal'
 import Settings from './pages/Settings'
 import VerifyEmail from './pages/VerifyEmail'
 import ResetPassword from './pages/ResetPassword'
+import ForgotPassword from './pages/ForgotPassword'
 import Chat from './pages/Chat'
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
@@ -37,6 +38,7 @@ function AppRoutes() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route
         path="/dashboard"
         element={
